905bdb2f2153360c0d2aef376645bfe92a1fab62
kkouer <kkouer@live.cn>
2016-01-22 16:23:18 +0800

    添加控件

---
+++ DoubelLabel.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MissionPlanner
+{
+    partial class DoubelLabel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelName.Font = new System.Drawing.Font("SimSun", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelName.Location = new System.Drawing.Point(0, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(49, 14);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "名称：";
+            // 
+            // labelValue
+            // 
+            this.labelValue.AutoSize = true;
+            this.labelValue.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelValue.Font = new System.Drawing.Font("SimSun", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelValue.Location = new System.Drawing.Point(75, 0);
+            this.labelValue.Name = "labelValue";
+            this.labelValue.Size = new System.Drawing.Size(35, 14);
+            this.labelValue.TabIndex = 2;
+            this.labelValue.Text = "数值";
+            // 
+            // DoubelLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelValue);
+            this.Controls.Add(this.labelName);
+            this.Name = "DoubelLabel";
+            this.Size = new System.Drawing.Size(110, 26);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelValue;
+    }
+}
---
+++ DoubelLabel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MissionPlanner
+{
+    public partial class DoubelLabel : UserControl
+    {
+        public DoubelLabel()
+        {
+            InitializeComponent();
+        }
+        [System.ComponentModel.Browsable(true)]
+        public string LabelName
+        {
+            get
+            {
+                return labelName.Text;
+            }
+            set
+            {
+                if (labelName.Text == value)
+                    return;
+                labelName.Text = value;
+            }
+        }
+        [System.ComponentModel.Browsable(true)]
+        public string LabelExtValue
+        {
+            get
+            {
+                return labelValue.Text;
+            }
+            set
+            {
+                if (labelValue.Text == value)
+                    return;
+                labelValue.Text = value;
+            }
+        }
+    }
+}
